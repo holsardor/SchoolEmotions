a3468d0d5a5761a70096fc73201354f248606816
holsardor <sardorxonsobirxon@gmail.com>
2023-08-08 18:40:30 +0500

    Project-Cleaned

--- MathApi/Services/IMathService.cs
+++
@@ -1,5 +0,0 @@
-namespace MathApi.Services;
-public interface IMathService
-{
-    Task<long> AddAsync(long a, long b, CancellationToken cancellationToken = default);
-}
--- SchoolEmotionsApi/Program.cs
+++ SchoolEmotionsApi/Program.cs
@@ -1,8 +1,5 @@
-using MathApi.Services;
-
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
-builder.Services.AddTransient<IMathService, MathService>();
 
 var app = builder.Build();
 app.MapControllers();
